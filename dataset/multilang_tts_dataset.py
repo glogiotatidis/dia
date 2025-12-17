45a9d903b0f2aaed946f0d8928233c4803a2f122
Giorgos Logiotatidis <seadog@sealabs.net>
2025-12-17 21:11:43 +0200

    Add mel spectrogram extraction to dataset for model compatibility

--- dataset/multilang_tts_dataset.py
+++ dataset/multilang_tts_dataset.py
@@ -3,13 +3,25 @@ import torch
 from torch.utils.data import Dataset
 from pathlib import Path
 import torchaudio
+import torchaudio.transforms as T
 
 class MultilangTTSDataset(Dataset):
-    def __init__(self, manifest_path, lang_vocab, sample_rate=22050):
+    def __init__(self, manifest_path, lang_vocab, sample_rate=22050, n_mels=80):
         self.data = json.load(open(manifest_path))
         self.lang_vocab = lang_vocab
         self.sample_rate = sample_rate
+        self.n_mels = n_mels
         self.phoneme_tokenizer = self._build_tokenizer()
+        
+        # Mel spectrogram transform
+        self.mel_transform = T.MelSpectrogram(
+            sample_rate=sample_rate,
+            n_fft=1024,
+            hop_length=256,
+            n_mels=n_mels,
+            norm="slaney",
+            mel_scale="slaney"
+        )
 
     def _build_tokenizer(self):
         from collections import Counter
@@ -31,7 +43,13 @@ class MultilangTTSDataset(Dataset):
         wav, sr = torchaudio.load(path)
         if sr != self.sample_rate:
             wav = torchaudio.functional.resample(wav, sr, self.sample_rate)
-        wav = wav.squeeze(0)
+        
+        # Compute mel spectrogram: (1, n_mels, time) -> (time, n_mels)
+        mel = self.mel_transform(wav)
+        mel = mel.squeeze(0).T  # (time, n_mels)
+        
+        # Log mel spectrogram
+        mel = torch.log(mel.clamp(min=1e-5))
 
         phoneme_ids = self.phoneme_tokenizer(sample["phonemes"])
         lang_token = f"<{sample['lang']}>"
@@ -40,7 +58,7 @@ class MultilangTTSDataset(Dataset):
 
         return {
             "input_ids": torch.tensor(input_ids, dtype=torch.long),
-            "waveform": wav,
+            "mel": mel,  # (time, n_mels)
             "lang": sample["lang"],
             "lang_token_id": lang_token_id,
             "path": str(path)
@@ -49,15 +67,17 @@ class MultilangTTSDataset(Dataset):
 def collate_fn(batch):
     from torch.nn.utils.rnn import pad_sequence
     input_seqs = [b["input_ids"] for b in batch]
-    waveforms = [b["waveform"] for b in batch]
+    mels = [b["mel"] for b in batch]  # List of (time, n_mels) tensors
+    
     padded_inputs = pad_sequence(input_seqs, batch_first=True, padding_value=0)
-    wav_lens = [len(w) for w in waveforms]
-    padded_audio = pad_sequence(waveforms, batch_first=True)
+    mel_lens = [m.shape[0] for m in mels]
+    padded_mels = pad_sequence(mels, batch_first=True)  # (B, max_time, n_mels)
 
     return {
         "input_ids": padded_inputs,
-        "audio": padded_audio,
-        "audio_lens": wav_lens,
+        "audio": padded_mels,  # (B, T, 80) - mel spectrograms
+        "audio_lens": mel_lens,
         "langs": [b["lang"] for b in batch],
-        "lang_token_ids": torch.tensor([b["lang_token_id"] for b in batch])
+        "lang_token_ids": torch.tensor([b["lang_token_id"] for b in batch]),
+        "paths": [b["path"] for b in batch]
     }
